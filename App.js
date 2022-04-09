9ae9ee2591487918bf2a85ea9d7d28bf6c782056
ujjual <ujjual72@gmail.com>
2022-04-09 22:27:15 +0530

    edit

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer,DefaultTheme } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>finish it faggot</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigator>
+        <Stack.Screen/>
+        <Stack.Screen/>
+      </StackNavigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
